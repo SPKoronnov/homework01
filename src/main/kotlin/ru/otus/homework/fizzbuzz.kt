9f83af82cb6fb652a28388aa951505cc6a005619
SPKoronnov <ctalk@mail.ru>
2026-02-18 22:29:48 +0300

    homework01: implement fizzbuzz and sumOfTwo

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,5 +2,22 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+
+    val arr: Array<String> = Array(size = n, init = { "" })
+
+    for (i in arr.indices) {
+
+        when {
+            (i % 3 == 0) && (i % 5 == 0)-> arr[i] = "FizzBuzz"
+            (i % 3 == 0) -> arr[i] = "Fizz"
+            (i % 5 == 0) -> arr[i] = "Buzz"
+
+
+            else -> arr[i] = i.toString()
+        }
+
+    }
+
+    return arr
+
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,39 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+
+    val n:Int = numbers.size
+    var i: Int
+    var j: Int
+    var flag: Boolean = false
+
+    var arr=IntArray(2)
+
+    i=0
+    while (i < n-1)  {
+        j=n-1
+        while ( j > i)  {
+
+            if ( numbers[i] + numbers[j] == target ) {
+
+                arr[0]=i
+                arr[1]=j
+                flag = true
+            }
+            j--
+        }
+
+        i++
+    } //while (i < n-2)..
+
+    if (flag) {
+
+        return arr
+    } else {
+
+        throw IllegalArgumentException("нет таких чисел")
+    }
+
+
+
 }
